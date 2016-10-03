eb2be0f76b943c3962bc61f16f8299d48739f08b
sztobar <bszewczyk91@gmail.com>
2016-10-04 00:44:44 +0200

    Componentify

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,18 @@ import WordForm from './views/word-form'
 declare var require: any
 require('./styles/main.css')
 
+Object.assign(window, {
+  xs,
+  list: {
+    next: v => {
+      debugger
+      console.log(`next: ${v.valueOf()}`)
+    },
+    error: v => console.log(`err: ${v}`),
+    complete: v => console.log(`complete: ${v}`)
+  }
+})
+
 type Sources = {
   DOM: DOMSource,
   HTTP: HTTPSource
@@ -27,9 +39,43 @@ type Model = {
   haben: boolean,
   sein: boolean
 }
+type setString = (v: string) => string
+
+function createWordForm(type, DOM, words$, mode$, answersVisible$, specialCharClicks$, resetFields$ : Stream<setString>) {
+  const modelProxy$ : Stream<setString> = xs.of(() => '')
+  const props$ = xs.combine(
+    xs.of(type),
+    words$.map(v => v[type]),
+    modelProxy$.fold((acc, fn: setString) => fn(acc), ''),
+    mode$,
+    answersVisible$
+  ).map(([type, answer, model, mode, answersVisible]) => ({type, answer, model, mode, answersVisible}))
+  const wordForm = WordForm({DOM, props$}, type)
+
+  const appendChar$ : Stream<setString> = wordForm.active$
+    .map(activeInput => {
+      // TODO crusade on if - eliminate it, deus vult
+      if (!activeInput) {
+        return xs.never()
+      }
+      return specialCharClicks$.map(char => v => v + char)
+    })
+    .flatten()
+
+  modelProxy$.imitate(
+    xs.merge(
+      wordForm.setValue$,
+      appendChar$,
+      resetFields$,
+    )
+  )
+
+  return Object.assign({}, wordForm, {model$: modelProxy$})
+}
 
 function main(sources: Sources): Sinks {
   const {DOM} = sources
+
   const specialCharClicks$ = sources.DOM.select('.add-char')
     .events('mousedown')
     .map((ev) => {
@@ -37,75 +83,37 @@ function main(sources: Sources): Sinks {
       return (ev.target as HTMLButtonElement).textContent
     })
 
-  const inputs$ = sources.DOM.select('.input')
 
-  const focusInputs$ = inputs$
-    .events('focus')
-    .map(ev => (ev.target as Element).id)
+  // const setCheckbox$ = sources.DOM.select('.checkbox')
+  //   .events('change')
+  //   .map((ev) => {
+  //     const el = (ev.target as HTMLInputElement)
+  //     return v => Object.assign({}, v, {[el.id]: el.checked})
+  //   })
 
-  const blurInputs$ = inputs$
-    .events('blur')
-    .mapTo(null)
+  // const checkboxesStartVal = {
+  //   haben: false,
+  //   sein: false
+  // }
 
-  const activeInput$ = xs.merge(focusInputs$, blurInputs$)
+  // const resetCheckbox$ = xs.merge(nextClick$, mode$)
+  //   .map(() => v => Object.assign({}, checkboxesStartVal))
 
-  const appendInput$ : Stream<string[]> = activeInput$
-    .map(activeInput => {
-      if (!activeInput) {
-        return xs.never()
-      }
-      return specialCharClicks$.map(char =>
-        v => Object.assign({}, v, {[activeInput]: v[activeInput] + char})
-      )
-    })
-    .flatten()
-
-  const setInput$ : Stream<(v: string) => string> = inputs$
-    .events('input')
-    .map(ev => {
-      const el = (ev.target as HTMLInputElement)
-      return v => Object.assign({}, v, {[el.id]: el.value})
-    })
+  // const checkboxModel$ = xs.merge(setCheckbox$, resetCheckbox$)
+  //   .fold((acc, fn) => fn(acc), Object.assign({}, checkboxesStartVal))
 
   const nextClick$ = sources.DOM.select('.next')
     .events('click')
     .mapTo('')
 
-  const mode$ = sources.DOM.select('.change-mode')
-    .events('click')
-    .map(ev => (ev.target as HTMLButtonElement).textContent)
-    .startWith('meaning')
+  const resetFieldsProxy$ = xs.never()
+  const resetFields$ = resetFieldsProxy$.map(() => () => '')
 
-  const inputsStartVal = {
-    infinitiv: '',
-    praterium: '',
-    partizip2: '',
-    meaning: ''
-  }
+  const answersVisibleProxy$ = xs.never()
+  const answersVisible$ = answersVisibleProxy$.startWith(false)
 
-  const resetInput$ = xs.merge(nextClick$, mode$)
-    .map(() => v => Object.assign({}, inputsStartVal))
-
-  const inputModel$ = xs.merge(setInput$, appendInput$, resetInput$)
-    .fold((acc: Object, fn) => fn(acc), Object.assign({}, inputsStartVal))
-
-  const setCheckbox$ = sources.DOM.select('.checkbox')
-    .events('change')
-    .map((ev) => {
-      const el = (ev.target as HTMLInputElement)
-      return v => Object.assign({}, v, {[el.id]: el.checked})
-    })
-
-  const checkboxesStartVal = {
-    haben: false,
-    sein: false
-  }
-
-  const resetCheckbox$ = xs.merge(nextClick$, mode$)
-    .map(() => v => Object.assign({}, checkboxesStartVal))
-
-  const checkboxModel$ = xs.merge(setCheckbox$, resetCheckbox$)
-    .fold((acc, fn) => fn(acc), Object.assign({}, checkboxesStartVal))
+  const modeProxy$ = xs.never()
+  const mode$ = modeProxy$.startWith('meaning')
 
   const nextWord$ = xs.merge(nextClick$, mode$)
 
@@ -115,7 +123,6 @@ function main(sources: Sources): Sinks {
     .map(words => {
       return nextWord$.map(() => {
         if (!words.length) {
-          setTimeout(() => location.reload(), 5000)
           return {meaning: 'wszystko!'}
         }
         const idx = Math.floor(Math.random() * words.length)
@@ -125,24 +132,45 @@ function main(sources: Sources): Sinks {
     })
     .flatten()
 
-  const model$ = xs.combine(inputModel$, checkboxModel$)
-    .map(([inputs, checkboxes]) => {
-      return Object.assign({}, inputs, checkboxes)
-    })
+  const infinitivWordForm = createWordForm('infinitiv', DOM, words$, mode$, answersVisible$, specialCharClicks$, resetFields$);
+  const prateriumWordForm = createWordForm('praterium', DOM, words$, mode$, answersVisible$, specialCharClicks$, resetFields$);
+  const partizip2WordForm = createWordForm('partizip2', DOM, words$, mode$, answersVisible$, specialCharClicks$, resetFields$);
+  const meaningWordForm = createWordForm('meaning', DOM, words$, mode$, answersVisible$, specialCharClicks$, resetFields$);
 
-  const enter$ = sources.DOM.select('.input, .checkbox')
-    .events('keydown')
-    .filter(ev => (ev as KeyboardEvent).keyCode === 13)
+  const modeImpl$ = xs.merge(
+    infinitivWordForm.mode$,
+    prateriumWordForm.mode$,
+    partizip2WordForm.mode$,
+    meaningWordForm.mode$,
+  )
+  modeProxy$.imitate(modeImpl$)
 
-    // TODO why new merge and not nextWord$
-  const answersState$ = xs.merge(enter$.mapTo(true), xs.merge(nextClick$, mode$).mapTo(false))
+  const answersVisibleImpl$ = xs.merge(
+    infinitivWordForm.enterPress$,
+    prateriumWordForm.enterPress$,
+    partizip2WordForm.enterPress$,
+    meaningWordForm.enterPress$,
+  )
+  answersVisibleProxy$.imitate(answersVisibleImpl$)
 
-  const state$ = xs.combine(model$, words$, mode$, answersState$)
+  const resetFieldsImpl$ = xs.merge(
+    mode$,
+    nextClick$
+  )
+  resetFieldsProxy$.imitate(resetFieldsImpl$)
 
-  const sinks = {
-    DOM: state$.map(([model, word, mode, answersVisible]) => {
-      const modelCopy = Object.assign({}, model, {[mode]: word[mode]})
-      const {infinitiv, praterium, partizip2, meaning, haben, sein} = modelCopy
+
+  const view$ = xs.combine(
+    infinitivWordForm.DOM,
+    prateriumWordForm.DOM,
+    partizip2WordForm.DOM,
+    meaningWordForm.DOM,
+  )
+
+  return {
+    DOM: view$.map(([infinitivVDom, prateriumVDom, partizip2VDom, meaningVDom]) => {
+      // const modelCopy = Object.assign({}, model, {[mode]: word[mode]})
+      // const {infinitiv, praterium, partizip2, meaning, haben, sein} = modelCopy
       return div([
         div([
           button('#ä.add-char', 'ä'),
@@ -152,38 +180,21 @@ function main(sources: Sources): Sinks {
           button('.next', {style: {float: 'right'}}, 'next')
         ]),
         div({style: {clear: 'both'}}, [
-          div(`.word-form.word-form--infinitiv`, [
-            button('.word-form__mode', {props: {disabled: mode === 'infinitiv', tabIndex: -1}}, 'infinitiv'),
-            input(`#infinitiv.word-form__input`, {attrs: {placeholder: 'infinitiv'}, props: {value: infinitiv}}),
-            div('.word-form__answer', {class: {'answer-hidden': !answersVisible}}, [word.infinitiv])
-          ]),
-          div(`.word-form.word-form--praterium`, [
-            button('.word-form__mode', {props: {disabled: mode === 'praterium', tabIndex: -1}}, 'praterium'),
-            input(`#praterium.word-form__input`, {attrs: {placeholder: 'praterium'}, props: {value: praterium}}),
-            div('.word-form__answer', {class: {'answer-hidden': !answersVisible}}, [word.praterium])
-          ]),
-          div(`.word-form.word-form--partizip2`, [
-            button('.word-form__mode', {props: {disabled: mode === 'partizip2', tabIndex: -1}}, 'partizip2'),
-            input(`#partizip2.word-form__input`, {attrs: {placeholder: 'partizip2'}, props: {value: partizip2}}),
-            div('.word-form__answer', {class: {'answer-hidden': !answersVisible}}, [word.partizip2])
-          ]),
-          span('.word-block', [
-            label(['h', input('#haben.checkbox', {attrs: {type: 'checkbox'}, props: {checked: haben}})]),
-            label(['s', input('#sein.checkbox', {attrs: {type: 'checkbox'}, props: {checked: sein}})]),
-            br(),
-            div({class: {'answer-hidden': !answersVisible}}, [`${word.haben && word.sein ? 'h, s' : word.haben ? 'h' : 's'}`])
-          ]),
-          div(`.word-form.word-form--meaning`, [
-            button('.word-form__mode', {props: {disabled: mode === 'meaning', tabIndex: -1}}, 'meaning'),
-            input(`#meaning.word-form__input`, {attrs: {placeholder: 'meaning'}, props: {value: meaning}}),
-            div('.word-form__answer', {class: {'answer-hidden': !answersVisible}}, [word.meaning])
-          ])
+          infinitivVDom,
+          prateriumVDom,
+          partizip2VDom,
+          // span('.word-block', [
+          //   label(['h', input('#haben.checkbox', {attrs: {type: 'checkbox'}, props: {checked: haben}})]),
+          //   label(['s', input('#sein.checkbox', {attrs: {type: 'checkbox'}, props: {checked: sein}})]),
+          //   br(),
+          //   div({class: {'answer-hidden': !answersVisible}}, [`${word.haben && word.sein ? 'h, s' : word.haben ? 'h' : 's'}`])
+          // ]),
+          meaningVDom,
         ])
       ])
     }),
     HTTP: xs.of({url: 'data.txt', category: 'data'})
   }
-  return sinks
 }
 
 window.onload = () =>
@@ -191,3 +202,54 @@ window.onload = () =>
     DOM: makeDOMDriver('#app'),
     HTTP: makeHTTPDriver()
   })
+
+function _main({DOM}) {
+  function Component({DOM, props$}, id) {
+    const setInput$ = DOM.select(`#${id}`)
+      .events('input')
+      .map(ev => (ev.target as HTMLInputElement).value)
+      .map(v => () => v)
+
+    const value$ = props$.map(v => v.model)
+      .fold((acc, fn: (string) => string) => fn(acc), '')
+      .startWith('')
+
+    return {
+      DOM: value$.map((value) => {
+        return div([
+          input(`#${id}`, {props: {value}}),
+          span([value])
+        ])
+      }),
+      setInput$
+    }
+  }
+
+  const clearInput$ = DOM.select('.clear')
+    .events('click')
+    .mapTo(() => '')
+  const stateProxy$ = xs.create()
+  const props$ = stateProxy$.map(model => ({model}))
+  const component = Component({DOM, props$}, 'one')
+  const state$: Stream<any> = xs.merge(clearInput$, component.setInput$)
+  stateProxy$.imitate(state$)
+
+  const state2Proxy$ = xs.create()
+  const props2$ = state2Proxy$.map(model => ({model}))
+  const component2 = Component({DOM, props$: props2$}, 'two')
+  const state2$: Stream<any> = xs.merge(clearInput$, component2.setInput$)
+  state2Proxy$.imitate(state2$)
+
+  const view$ = xs.combine(component.DOM, component2.DOM)
+
+  return {
+    DOM: view$.map(([view, view2]) => {
+      return div([
+        view,
+        view2,
+        button('.clear', 'clear')
+      ])
+    }),
+    HTTP: xs.of({url: 'data.txt', category: 'data'})
+  }
+}
--- src/views/word-form.ts
+++ src/views/word-form.ts
@@ -3,13 +3,13 @@ import xs, {Stream} from 'xstream';
 import {DOMSource} from '@cycle/dom/xstream-typings';
 import isolate from '@cycle/isolate';
 
-export default sources => isolate(WordForm)(sources);
+export default (sources, type) => isolate(WordForm)(sources, type);
 
 type Props = {
   type: string,
   answer: string,
   model: string,
-  currentMode: string,
+  mode: string,
   answersVisible: boolean
 };
 type Sources = {
@@ -18,13 +18,13 @@ type Sources = {
 };
 type Sinks = {
   DOM: Stream<VNode>,
-  value$: Stream<string>,
+  setValue$: Stream<(_: string) => string>,
   mode$: Stream<string>,
   active$: Stream<string>,
   enterPress$: Stream<any>
 };
 
-function WordForm({DOM: domSource, props$} : Sources) : Sinks {
+function WordForm({DOM: domSource, props$} : Sources, type) : Sinks {
 
   const input$ = domSource.select('.word-form__input')
 
@@ -42,22 +42,24 @@ function WordForm({DOM: domSource, props$} : Sources) : Sinks {
     .events('keydown')
     .filter(ev => (ev as KeyboardEvent).keyCode === 13)
 
-  const value$ = input$
+  const setValue$ = input$
     .events('input')
     .map(ev => (ev.target as HTMLInputElement).value)
+    .map((v : string) => (_: string) => v)
 
   const mode$ = domSource.select('.word-form__mode')
     .events('click')
-    .map(ev => props$.map(v => v.type))
-    .flatten()
+    .map(ev => type)//props$.map(v => v.type))
+    //.flatten()
 
 
   const view$ = props$.map(props => {
-    const {type, answer, model, currentMode, answersVisible} = props;
+    const {type, answer, model, mode, answersVisible} = props;
+    const value = mode !== type ? model : answer; 
 
     return div(`.word-form.word-form--${type}`, [
-      button('.word-form__mode', {props: {disabled: currentMode === type, tabIndex: -1}}, type),
-      input(`#${type}.word-form__input`, {attrs: {placeholder: type}, props: {value: model}}),
+      button('.word-form__mode', {props: {disabled: mode === type, tabIndex: -1}}, type),
+      input(`#${type}.word-form__input`, {attrs: {placeholder: type}, props: {value}}),
       div('.word-form__answer', {class: {'answer-hidden': !answersVisible}}, [answer])
     ])
   })
@@ -68,7 +70,7 @@ function WordForm({DOM: domSource, props$} : Sources) : Sinks {
 
   return {
     DOM: view$,
-    value$,
+    setValue$,
     mode$,
     active$,
     enterPress$
